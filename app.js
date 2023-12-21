acd6008d87d0384c47c8d55a55dbb98441e592e0
daniyal <syeddaniyal995@gmail.com>
2023-12-21 09:43:48 +0000

    solution

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const addDays = required('date-fns/addDays')
+const app = express()
+
+app.get("/",(request,response)=>{
+   const date = addDays(new Date(),100)
+   response.send(`${date.getDate()}/${date.getMonth() +1}/${date.getFullYear()}`);
+})
+
+module.exports = app
